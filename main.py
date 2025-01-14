b20d6bd4cec4b176f0ffe9b6ff798bdcac46790d
lexynovicecoder <alexandradidieye21@gmail.com>
2025-01-14 23:13:30 +0000

    created instance for BookModel and created endpoint to handle POST requests. Haven't set up configuration details yet

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 from fastapi import FastAPI
 import argparse
 import uvicorn
+from sqlmodel import Session
+from models import *
 
 app = FastAPI()
-@app.get('/')
 
-def hello_word():
-    return {"message": "Hello World"}
+
+@app.get('/')
+def example():
+    return {"title": "Example"}
+
+@app.post("/books")
+def create_book(book: BookModel):
+    with Session() as session:  # a session is used to manage database interactions(adding,querying,updating etc)
+        db_item = book
+        session.add(db_item)  # adds BookModel instance to session but it is pending
+        session.commit()  # writes all pending changes to database
+        session.refresh(db_item)  # refresh instance
+        return db_item
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run FastAPI with optional file watching.")
     parser.add_argument("--watch", action="store_true", help="Enable auto-reload for the server.")
     args = parser.parse_args()
 
-
     uvicorn.run(
-            app,
-            host="127.0.0.1",
-            port=8000,
-            reload=args.watch  # Enable reload if --watch flag is passed
-        )
+        app,
+        host="127.0.0.1",
+        port=8000,
+        reload=args.watch  # Enable reload if --watch flag is passed
+    )
---
+++ models.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from sqlmodel import Field, SQLModel, Session, create_engine, select
+
+
+# Model: Pydantic Model + SQL Model
+
+class BookModel(SQLModel):
+    title: str = Field(index=True)  # setting index to true creates an index on the title column
+    # making it easier to query for book titles
+    author: str
+    isbn: str = Field(min_length=4, max_length=5, default_factory=lambda: "0011")  # within the field are min_length
+    # and max_length that ensures isbn conforms to a specific format default_factory ensures new instances of
+    # BookModel have a predefined value if none is explicitly set
+    description: Optional[str]  # making this attribute optional. No error is raised when not set
