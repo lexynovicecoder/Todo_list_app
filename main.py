f2ecf5d6fa6b255548e79b89ee2e1ff0bb35a248
lexynovicecoder <alexandradidieye21@gmail.com>
2025-01-19 20:19:27 +0000

    created enpoint for patch(complete and undo task) and delete.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Response, status, Depends, HTTPException
 import argparse
 import uvicorn
 from sqlmodel import Session, create_engine
@@ -36,55 +36,89 @@ def example():
     return {"Title": "Todo list"}
 
 
-@app.get("/tasks", response_model=List[TaskRead])
+@app.get("/tasks", response_model=List[TodoList])
 def read_tasks(session: Session = Depends(get_session)):
-    tasks = session.exec(select(Task)).all()  # Query the database for all Task records
-    return [TaskRead.from_orm(task) for task in tasks]  # Convert Task models to TaskRead models
+    tasks = session.exec(select(TodoList)).all()  # Query the database for all Task records
+    return tasks
 
 
-@app.post("/tasks", response_model=TaskRead)
-def create_task(task: TaskCreate, session: Session = Depends(get_session)):
-    db_item = Task.model_validate(task)
+@app.post("/tasks", response_model=TodoList, status_code = 201 )
+def create_task(task: TaskCreateDTO,response: Response, session: Session = Depends(get_session)):
+    db_item = TodoList(**task.model_dump())  # created_at and updated_at will be set automatically by the event
     session.add(db_item)
     session.commit()
     session.refresh(db_item)
+    response.status_code = status.HTTP_201_CREATED
     return db_item
 
 
-@app.get("/tasks/{task_id}", response_model=TaskRead)
+@app.get("/tasks/{task_id}", response_model=TodoList)
 def read_task(task_id: int, session: Session = Depends(get_session)):
-    task = session.get(Task, task_id)
+    task = session.get(TodoList, task_id)
     if not task:
         raise HTTPException(status_code=404, detail="Task Not Found!")
     return task
-#
-# @app.patch("/books/{book_id}", response_model=Book)
-# def update_book(book_id: int, book: Book):
-#     with Session(engine) as session:
-#         book_item = session.get(Book, book_id)
-#         if not book_item:
-#             raise HTTPException(status_code=404, detail="Book Not Found!")
-#         book_data = book.dict(exclude_unset=True)
-#         for key, value in book_data.items():
-#             setattr(book_item, key, value)
-#         session.add(book_item)
-#         session.commit()
-#         session.refresh(book_item)
-#         return book_item
-#
 
-@app.delete("/tasks/{task_id}")
-def delete_task(task_id: int, session: Session = Depends(get_session)):
-    task = session.get(Task, task_id)
+@app.put("/tasks/{task_id}", response_model=TodoList)
+def update_task(task_id: int, task: TaskCreateDTO, session: Session = Depends(get_session)):
+    task_item = session.get(TodoList, task_id)
+    if not task_item:
+        raise HTTPException(status_code=404, detail="Task Not Found!")
+    task_data = task.model_dump(exclude_unset=False)
+    for key, value in task_data.items():
+        setattr(task_item, key, value)
+    task_item.updated_at = datetime.now()
+
+    session.add(task_item)
+    session.commit()
+    session.refresh(task_item)
+    return task_item
+
+@app.patch("/tasks/{task_id}/complete", response_model=TodoList)
+def complete_task(task_id: int, session: Session = Depends(get_session)):
+    task_item = session.get(TodoList, task_id)
+    if not task_item:
+        raise HTTPException(status_code=404, detail="Task Not Found!")
+    if task_item.is_completed:
+        raise HTTPException(status_code=401, detail="Already Completed")
+    task_item.updated_at = datetime.now()
+    task_item.is_completed = True
+
+    session.add(task_item)
+    session.commit()
+    session.refresh(task_item)
+    return task_item
+
+@app.patch("/tasks/{task_id}/undo", response_model=TodoList)
+def undo_task(task_id: int, session: Session = Depends(get_session)):
+    task_item = session.get(TodoList, task_id)
+    if not task_item:
+        raise HTTPException(status_code=404, detail="Task Not Found!")
+    if not task_item.is_completed:
+        raise HTTPException(status_code=401, detail="Cannot Undo")
+    task_item.updated_at = datetime.now()
+    task_item.is_completed = False
+
+    session.add(task_item)
+    session.commit()
+    session.refresh(task_item)
+    return task_item
+
+
+@app.delete("/tasks/{task_id}", status_code = 204)
+def delete_task(task_id: int, response: Response,session: Session = Depends(get_session)):
+    task = session.get(TodoList, task_id)
     if not task:
         raise HTTPException(status_code=404, detail="Task hasn't been created")
     session.delete(task)
     session.commit()
-    return {"ok": True}
-# #
+    response.status_code = status.HTTP_204_NO_CONTENT
+    return response.status
+
 #
-#
-#
+
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run FastAPI with optional file watching.")
--- models.py
+++ models.py
@@ -1,31 +1,26 @@
 from typing import Optional
 from sqlmodel import Field, SQLModel, Session, create_engine, select
-from datetime import datetime
+from datetime import datetime,timedelta
+from pydantic import BaseModel
 
 
 
-class TodoList(SQLModel):
+class TodoList(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
     name: str = Field(index=True)
     is_completed: bool = Field(default=False)  # Default value for is_completed
-    deadline: Optional[datetime] = Field(default=None)
     description: Optional[str] = None
     created_at: datetime = Field(default_factory=datetime.now)  # Default factory for current timestamp
-    updated_at: Optional[datetime] = Field(default=None)
+    deadline: Optional[datetime] = Field(default=datetime.now()+timedelta(days=1))
+
+    updated_at: Optional[datetime] = Field(default_factory=None)
 
 
-class Task(TodoList, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
 
 
-class TaskCreate(SQLModel):
-    name: str
-    is_completed: Optional[bool] = Field(default=False)
-    deadline: Optional[datetime] = None
+class TaskCreateDTO(BaseModel):
+    name: str 
+    is_completed: bool = Field(default=False)  # Default value for is_completed   
+    deadline: Optional[datetime] = Field(default=datetime.now()+timedelta(days=1))
     description: Optional[str] = None
-
-
-class TaskRead(TodoList):
-    id: int
-    updated_at: Optional[datetime] = None  # Explicitly set updated_at default to None
-
 
