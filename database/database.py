5f57bce1f26eab0f920f7271d549bc6da7fcee5f
Alexandra <didieyealexandra@gmail.com>
2025-01-29 09:17:14 -0800

    created new .env file

--- database/database.py
+++ database/database.py
@@ -2,6 +2,6 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-db_type = os.getenv("DB_TYPE")  # Default to sqlite if not set
-db_name = os.getenv("DB_NAME")  # Default to database.db if not set
-DATABASE_URL = f"{db_type}:///./database/{db_name}"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+print(DATABASE_URL)
