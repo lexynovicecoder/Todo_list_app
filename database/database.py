618a1a3d0765fc57e861016385b8400cb48c509f
lexynovicecoder <alexandradidieye21@gmail.com>
2025-01-29 14:47:17 +0000

    created services for task routers and created DTO for updated tasks

--- database/database.py
+++ database/database.py
@@ -1,3 +1,4 @@
+from sqlmodel import create_engine
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -5,3 +6,6 @@ load_dotenv()
 db_type = os.getenv("DB_TYPE")  # Default to sqlite if not set
 db_name = os.getenv("DB_NAME")  # Default to database.db if not set
 DATABASE_URL = f"{db_type}:///./database/{db_name}"
+
+connect_args = {"check_same_thread": False}  # enables multi-thread access
+engine = create_engine(DATABASE_URL, echo=True, connect_args=connect_args)
--- routers/todoList_routers.py
+++ routers/todoList_routers.py
@@ -1,35 +1,23 @@
-from fastapi import  Response, Depends
+from fastapi import  Depends
 from fastapi import APIRouter
 from Models.models import *
 from DTOs.dtos import TodoListCreateDTO,TodoListResponseDTO
 from datetime import datetime
 from database.database import *
 from typing import List
-from routers.task_routers import engine
+from services.todo_services import engine
 from sqlalchemy import event
-from services.todolist_services import TodolistServices  # Assuming the service is in a services folder
+from services.todolist_services import TodolistServices, todolist_service  # Assuming the service is in a services folder
 
 
 router2 = APIRouter(tags=["TodoLists"])
 
-def get_session():
-    with Session(engine) as session:
-        yield session  # Yield the session for use in endpoints
 
-
-def todolist_service(response: Response, session: Session = Depends(get_session)):
-    return TodolistServices(session=session, response=response)
-    
 
 @event.listens_for(TodoList, "before_update")
 def update_updated_at(mapper, connection, target):
     target.updated_at = datetime.now()
 
-
-@router2.post("",response_model=TodoList, status_code = 201 )
-def create_todolist(task: TodoListCreateDTO,todolist_service: TodolistServices = Depends(todolist_service)):
-    db_item = todolist_service.create_todolist(task)
-    return db_item
 
 @router2.get("", response_model=List[TodoList])
 def read_todoLists(todolist_service: TodolistServices = Depends(todolist_service)):
@@ -42,7 +30,10 @@ def read_todolist(todolist_id: int, todolist_service: TodolistServices = Depends
     todolist = todolist_service.read_todolist(todolist_id)
     return todolist
 
-    
+@router2.post("",response_model=TodoList, status_code = 201 )
+def create_todolist(task: TodoListCreateDTO,todolist_service: TodolistServices = Depends(todolist_service)):
+    db_item = todolist_service.create_todolist(task)
+    return db_item  
 
 
 @router2.put("/{todolist_id}", response_model=TodoList)
